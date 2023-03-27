7cc41e19d9997638c3a3b641e5e2c74510ed5666
Sam Shenker <your@email.here>
2023-03-27 12:46:06 -0400

    working base dockerfile

--- api/main.py
+++ api/main.py
@@ -14,6 +14,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 @app.get("/api/launch-details")
 def launch_details():
