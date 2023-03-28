8bc173b22c918bfc3c026f590ccb1f3cc7f1dbeb
Logan <logco4@gmail.com>
2023-03-28 14:01:13 -0400

    Added some of the code from lecture, may have to translate for PostgreSQL, updated jwtdown and FastAPI versions

--- api/queries/accounts.py
+++ api/queries/accounts.py
@@ -21,9 +21,22 @@ class AccountOutWithPassword(AccountOut):
     hashed_password: str
 
 class AccountQueries:
-    def get(self, email: str) -> AccountOutWithPassword:
-        pass
+    COLLECTION = "accounts"
+
+    def get(self, username: str) -> AccountOutWithPassword:
+        result = self.collection.find_one({"username": username.lower()})
+        if result is None:
+            return None
+        result["id"] = str(result["_id"])
+        return AccountOutWithPassword(**result)
 
     def create(self, info: AccountIn, hashed_password: str) -> AccountOutWithPassword:
-        print("-----------", info, hashed_password)
-        pass
+        info.username = info.username.lower()
+        account = info.dict()
+        del account["password"]
+        account["hashed_password"] = hashed_password
+        if self.get(account["username"]) is not None:
+            raise DuplicateAccountError
+        self.collection.insert_one(account)
+        account["id"] = str(account["_id"])
+        return AccountOutWithPassword(**account)
