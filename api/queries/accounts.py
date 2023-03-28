e9e68a76b8fc96ce58c243319911de0b38a872ef
Daniel Lee <danieltlee18@gmail.com>
2023-03-28 18:36:14 -0400

    migration tables for auth and auth code(WIP)

--- api/queries/accounts.py
+++ api/queries/accounts.py
@@ -1,7 +1,7 @@
 # authenticator.py
 # from fastapi import Depends
 from pydantic import BaseModel
-
+from routers.pool import pool
 
 
 class DuplicateAccountError(ValueError):
@@ -21,22 +21,42 @@ class AccountOutWithPassword(AccountOut):
     hashed_password: str
 
 class AccountQueries:
-    COLLECTION = "accounts"
+    # COLLECTION = "accounts"
 
     def get(self, username: str) -> AccountOutWithPassword:
-        result = self.collection.find_one({"username": username.lower()})
-        if result is None:
-            return None
-        result["id"] = str(result["_id"])
-        return AccountOutWithPassword(**result)
+        # result = self.collection.find_one({"username": username.lower()})
+        # if result is None:
+        #     return None
+        # result["id"] = str(result["_id"])
+        # return AccountOutWithPassword(**result)
+        pass
 
     def create(self, info: AccountIn, hashed_password: str) -> AccountOutWithPassword:
         info.username = info.username.lower()
         account = info.dict()
         del account["password"]
         account["hashed_password"] = hashed_password
-        if self.get(account["username"]) is not None:
-            raise DuplicateAccountError
-        self.collection.insert_one(account)
-        account["id"] = str(account["_id"])
+        with pool.connection() as conn:
+            with conn.cursor() as curs:
+                curs.execute(
+                    """
+                    SELECT username
+                    FROM accounts
+                    WHERE username = %s
+                    """,
+                    [account["username"]]
+                )
+                if curs.fetchone() is not None:
+                    raise DuplicateAccountError
+
+                output = curs.execute(
+                    """
+                    INSERT INTO accounts (username, email, hashed_password)
+                    VALUES (%s, %s, %s)
+                    RETURNING id
+                    """,
+                    [account["username"], account["email"], account["hashed_password"]]
+                )
+                account["id"] = output.fetchone()[0]
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", output.fetchall())
         return AccountOutWithPassword(**account)
