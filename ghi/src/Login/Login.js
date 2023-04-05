2b706f7724f86ea677e85d47edc60a845ed0c93b
Sam Shenker <your@email.here>
2023-04-05 17:45:33 -0400

    added redux store state for login form

--- ghi/src/Login/Login.js
+++ ghi/src/Login/Login.js
@@ -1,36 +1,55 @@
 import "./Login.css";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {handleUsernameChange, handlePasswordChange}  from "../features/auth/loginSlice"
 
 const Login = () => {
+    const dispatch = useDispatch();
+    const { fields } = useSelector((state) => state.login);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("handleSubmit");
+    };
+
   return (
-    <section className="login-wrapper">
-      <div className="login-card">
-        <h1>Login</h1>
-        <form className="login-form">
-          <div className="inputs">
-            <label className="login-labels" for="username">
-              Username
-            </label>
-            <input
-              className="login-input"
-              placeholder="username"
-              id="username"
-              required
-            ></input>
-            <label className="login-labels" for="password">
-              Password
-            </label>
-            <input
-              className="login-input"
-              placeholder="password"
-              type="password"
-              id="password"
-              required
-            ></input>
+      <section className="login-wrapper">
+          <div className="login-card">
+              <h1>Login</h1>
+              <form className="login-form">
+                  <div className="inputs">
+                      <label className="login-labels" for="username">
+                          Username
+                      </label>
+                      <input
+                          className="login-input"
+                          placeholder="username"
+                          id="username"
+                          required
+                          value={fields.username}
+                          onChange={(e) =>
+                              dispatch(handleUsernameChange(e.target.value))
+                          }
+                      ></input>
+                      <label className="login-labels" for="password">
+                          Password
+                      </label>
+                      <input
+                          className="login-input"
+                          placeholder="password"
+                          type="password"
+                          id="password"
+                          required
+                          value={fields.password}
+                          onChange={(e) =>
+                              dispatch(handlePasswordChange(e.target.value))
+                          }
+                      ></input>
+                  </div>
+                  <button className="login-button">Log In</button>
+              </form>
           </div>
-          <button className="login-button">Log In</button>
-        </form>
-      </div>
-    </section>
+      </section>
   );
 };
 export default Login;
--- ghi/src/app/store.js
+++ ghi/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import signupReducer from "../features/auth/signupSlice";
+import loginReducer from "../features/auth/loginSlice"
 import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
     reducer: {
         signup: signupReducer,
+        login: loginReducer
     },
 });
 
