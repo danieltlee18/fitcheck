27b794e35df67fdb9a7b35f7f67b8d342a6bc144
Logan <logco4@gmail.com>
2023-04-06 16:43:27 -0400

    implemented front-end login functinality

--- ghi/src/Login/Login.js
+++ ghi/src/Login/Login.js
@@ -1,55 +1,70 @@
 import "./Login.css";
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {handleUsernameChange, handlePasswordChange}  from "../features/auth/loginSlice"
+import {handleUsernameChange, handlePasswordChange, reset}  from "../features/auth/loginSlice"
+import { useLoginMutation } from "../services/auth";
+import { useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
     const dispatch = useDispatch();
+    const [login] = useLoginMutation()
     const { fields } = useSelector((state) => state.login);
+    const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log("handleSubmit");
+        try {
+            const result = await login(fields)
+            dispatch(reset())
+            console.log(result)
+            if (result.data?.access_token) {
+                navigate("/dashboard");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+
     };
 
-  return (
-      <section className="login-wrapper">
-          <div className="login-card">
-              <h1>Login</h1>
-              <form className="login-form">
-                  <div className="inputs">
-                      <label className="login-labels" for="username">
-                          Username
-                      </label>
-                      <input
-                          className="login-input"
-                          placeholder="username"
-                          id="username"
-                          required
-                          value={fields.username}
-                          onChange={(e) =>
-                              dispatch(handleUsernameChange(e.target.value))
-                          }
-                      ></input>
-                      <label className="login-labels" for="password">
-                          Password
-                      </label>
-                      <input
-                          className="login-input"
-                          placeholder="password"
-                          type="password"
-                          id="password"
-                          required
-                          value={fields.password}
-                          onChange={(e) =>
-                              dispatch(handlePasswordChange(e.target.value))
-                          }
-                      ></input>
-                  </div>
-                  <button className="login-button">Log In</button>
-              </form>
-          </div>
-      </section>
-  );
+    return (
+        <section className="login-wrapper">
+            <div className="login-card">
+                <h1>Login</h1>
+                <form onSubmit={handleSubmit} className="login-form">
+                    <div className="inputs">
+                        <label className="login-labels" for="username">
+                            Username
+                        </label>
+                        <input
+                            className="login-input"
+                            placeholder="username"
+                            id="username"
+                            required
+                            value={fields.username}
+                            onChange={(e) =>
+                                dispatch(handleUsernameChange(e.target.value))
+                            }
+                        ></input>
+                        <label className="login-labels" for="password">
+                            Password
+                        </label>
+                        <input
+                            className="login-input"
+                            placeholder="password"
+                            type="password"
+                            id="password"
+                            required
+                            value={fields.password}
+                            onChange={(e) =>
+                                dispatch(handlePasswordChange(e.target.value))
+                            }
+                        ></input>
+                    </div>
+                    <button className="login-button">Log In</button>
+                </form>
+            </div>
+        </section>
+    );
 };
 export default Login;
---
+++ ghi/src/services/auth.js
@@ -0,0 +1,57 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+
+export const authApi = createApi({
+    reducerPath: 'authApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: `${process.env.REACT_APP_FitCheck_API_HOST}`,
+        credentials: "include" // sends cookie to FastAPI
+    }),
+    endpoints: (builder) => ({
+        getAccount: builder.query({
+            query: () => '/token',
+            transformResponse: (response) => response?.account,
+            providesTags: ['Account']
+        }),
+        login: builder.mutation({
+            query: (body) => {
+                const formData = new FormData()
+                console.log(process.env.REACT_APP_FitCheck_API_HOST);
+                formData.append('username', body.username)
+                formData.append('password', body.password)
+                return {
+                    url: '/token',
+                    method: 'POST',
+                    body: formData,
+                    credentials: "include"
+                }
+            },
+            invalidatesTags: ['Account']
+        }),
+        logout: builder.mutation({
+            query: () => ({
+                url: '/token',
+                method: 'DELETE'
+            }),
+            invalidatesTags: ['Account']
+        }),
+
+        signup: builder.mutation({
+            query: (body) => {
+                const formData = new FormData()
+                formData.append('username', body.username)
+                formData.append('email', body.email)
+                formData.append('password', body.password)
+                return {
+                    url: '/api/accounts',
+                    method: 'POST',
+                    body: formData,
+                    credentials: "include"
+                }
+            },
+            invalidatesTags: ['Account']
+        })
+    })
+})
+
+export const { useGetAccountQuery, useLogoutMutation, useLoginMutation } = authApi;
