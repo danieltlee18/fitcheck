ec113304efd7b22cae2946673ece2842c9d805a7
Sam Shenker <your@email.here>
2023-04-10 11:46:46 -0400

    Updated work journal

--- ghi/src/Login/Login.js
+++ ghi/src/Login/Login.js
@@ -12,44 +12,44 @@ const Login = () => {
         console.log("handleSubmit");
     };
 
-  return (
-      <section className="login-wrapper">
-          <div className="login-card">
-              <h1>Login</h1>
-              <form className="login-form">
-                  <div className="inputs">
-                      <label className="login-labels" for="username">
-                          Username
-                      </label>
-                      <input
-                          className="login-input"
-                          placeholder="username"
-                          id="username"
-                          required
-                          value={fields.username}
-                          onChange={(e) =>
-                              dispatch(handleUsernameChange(e.target.value))
-                          }
-                      ></input>
-                      <label className="login-labels" for="password">
-                          Password
-                      </label>
-                      <input
-                          className="login-input"
-                          placeholder="password"
-                          type="password"
-                          id="password"
-                          required
-                          value={fields.password}
-                          onChange={(e) =>
-                              dispatch(handlePasswordChange(e.target.value))
-                          }
-                      ></input>
-                  </div>
-                  <button className="login-button">Log In</button>
-              </form>
-          </div>
-      </section>
-  );
+    return (
+        <section className="login-wrapper">
+            <div className="login-card">
+                <h1>Login</h1>
+                <form className="login-form">
+                    <div className="inputs">
+                        <label className="login-labels" for="username">
+                            Username
+                        </label>
+                        <input
+                            className="login-input"
+                            placeholder="username"
+                            id="username"
+                            required
+                            value={fields.username}
+                            onChange={(e) =>
+                                dispatch(handleUsernameChange(e.target.value))
+                            }
+                        ></input>
+                        <label className="login-labels" for="password">
+                            Password
+                        </label>
+                        <input
+                            className="login-input"
+                            placeholder="password"
+                            type="password"
+                            id="password"
+                            required
+                            value={fields.password}
+                            onChange={(e) =>
+                                dispatch(handlePasswordChange(e.target.value))
+                            }
+                        ></input>
+                    </div>
+                    <button className="login-button">Log In</button>
+                </form>
+            </div>
+        </section>
+    );
 };
 export default Login;
