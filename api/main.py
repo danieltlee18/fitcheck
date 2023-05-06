f63c8fc0d9c6565def1a7e2e977867f4863c44cd
Amro Salha <amro.salha@icloud.com>
2023-05-06 15:26:07 -0400

    flake8 styling added

--- api/main.py
+++ api/main.py
@@ -18,7 +18,6 @@ app.add_middleware(
         'https://fitcheck.one',
         'https://lads51.gitlab.io',
         'https://www.fitcheck.one',
-
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -31,17 +30,20 @@ app.add_middleware(
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/token", response_model=AccountOut | HttpError)
 async def get_account(
-    repo: AccountQueries = Depends(),
-    curr_account: dict = Depends(authenticator.get_current_account_data),
+        repo: AccountQueries = Depends(),
+        curr_account: dict = Depends(authenticator.get_current_account_data),
 ) -> AccountOut:
     response = AccountOut.from_orm(curr_account)
     access_token = authenticator.create_access_token(curr_account)
     response.headers["Set-Cookie"] = f"access_token={access_token}; HttpOnly; Secure; SameSite=None"
     return response
 
+
 app.include_router(outfits.router, tags=["Outfits"])
 app.include_router(authenticator.router, tags=["Authentication"])
 app.include_router(accounts.router, tags=["Accounts"])
 app.include_router(ratings.router, tags=["Ratings"])
+
