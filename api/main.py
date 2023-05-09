51dd6679d425b4f86c31f0070f0a708595108fa5
Sam Shenker <sshenker88@gmail.com>
2023-05-09 13:35:10 -0400

    fixed typo in cors

--- api/main.py
+++ api/main.py
@@ -34,7 +34,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
     allow_methods=["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"],
     expose_headers=["pragma"],
     max_age=315576000
